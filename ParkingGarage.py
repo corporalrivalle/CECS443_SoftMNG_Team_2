c24cea15d9826b9f55e690502b5e2757fb56346c
JOHNNIE MARES <johnniemares@JOHNNIEs-MBP.lan>
2023-04-20 11:08:36 -0700

    implemented repating menue until user chooses to cancel

--- ParkingGarage.py
+++ ParkingGarage.py
@@ -29,43 +29,57 @@ def main ():
     cap=0
     limit = 16
     end = 4
-    onsite=False
-    online = False
-    print ("Welcome to the Automated parking garage.\n")
-
-    print("please select an option\n")
-    print("1: purchase onsite permit\n")
-    print("2: make Online Reservation\n")
-    my_input = int(input())
-
-    match my_input:
-        case 1:
-            onsite = True
-            print("you have chosen to purchase an a parking permit onsite\n")
-            print("let me check if we are at capacity...\n")
-            if capacity(cap,limit) > 0:
-                print("there is not sufficent capcity in the structure to allow you to choose a spot, we will choose one for you\n")
-            elif capacity(cap,limit) < 0:
-                print("ok lets find you the perfect spot\n")
-                cap += 1
-            else:
-                print("Sorry this lot if full, please try again later, goodbye :)\n")
-        
-            
-            
     
-        case 2:
-            print("you have chosen to make an online reservation\n")
-            print("let me check if we are at capacity...\n")
-            if capacity(cap,limit) > 0:
-                print("there is not sufficent capcity in the structure to allow you to choose a spot, we will choose one for you\n")
-            elif capacity(cap,limit) < 0:
-                print("ok lets find you the perfect spot\n")
-                cap += 1
-            else:
-                print("Sorry this lot if full, please try again later, goodbye :)\n")
-            
+    while 1:
+        onsite=False
+        online = False
+        leave = False
+        print ("Welcome to the Automated parking garage.\n")
 
+        print("please select an option\n")
+        print("1: purchase onsite permit\n")
+        print("2: make Online Reservation\n")
+        print("3: cancel request\n")
+        my_input = int(input())
+
+        match my_input:
+            case 1:
+                onsite = True
+                print("you have chosen to purchase an a parking permit onsite\n")
+                print("let me check if we are at capacity...\n")
+                if capacity(cap,limit) > 0:
+                    print("there is not sufficent capcity in the structure to allow you to choose a spot, we will choose one for you\n")
+                elif capacity(cap,limit) <= 0:
+                    print("ok lets find you the perfect spot\n")
+                    cap += 1
+                else:
+                    print("Sorry this lot if full, please try again later, goodbye :)\n")
+        
+            case 2:
+                online = True
+                print("you have chosen to make an online reservation\n")
+                print("let me check if we are at capacity...\n")
+                if capacity(cap,limit) > 0:
+                    print("there is not sufficent capcity in the structure to allow you to choose a spot, we will choose one for you\n")
+                elif capacity(cap,limit) <= 0:
+                    print("ok lets find you the perfect spot\n")
+                    cap += 1
+                else:
+                    print("Sorry this lot if full, please try again later, goodbye :)\n")
+
+            case 3:
+                print("you have chosen to cancel your request, goodbye :)")
+                leave = True
+                
+        
+        if onsite == True:
+            print("onsite is true\n")
+        elif online == True:
+            print("online reservation is true\n")
+        elif leave == True:
+            online =False
+            onsite = False
+            break
     
 
   

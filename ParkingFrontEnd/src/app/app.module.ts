e1eb15019382a8e840fee6a953d96071d39308f9
Derek Zhang <58023992+corporalrivalle@users.noreply.github.com>
2023-04-24 17:58:52 -0700

    Added angular component

---
+++ ParkingFrontEnd/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ViewLotsComponent } from './view-lots/view-lots.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomepageComponent } from './homepage/homepage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ViewLotsComponent,
+    LoginComponent,
+    SignupComponent,
+    HomepageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

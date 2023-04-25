e6547e672832cf39d3129a9202f5e4857dd4f106
Derek Zhang <58023992+corporalrivalle@users.noreply.github.com>
2023-04-24 18:51:02 -0700

    frontend updates

--- ParkingFrontEnd/src/app/app.module.ts
+++ ParkingFrontEnd/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {HttpClientModule} from '@angular/common/http'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ViewLotsComponent } from './view-lots/view-lots.component';
@@ -18,7 +20,8 @@ import { HomepageComponent } from './homepage/homepage.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

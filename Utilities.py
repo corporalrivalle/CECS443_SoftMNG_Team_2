5ada0ddc044ce927e472332c475fd8596b740e97
yakaijo <thongdatpro123@gmail.com>
2023-04-30 20:22:19 -0700

    Changed connection string to cloud based

--- Utilities.py
+++ Utilities.py
@@ -7,11 +7,11 @@ class Utilities:
     # startup - creates the connection and returns the database client."""
     @staticmethod
     def startup():
-        print ("--------------------------------------------")
-        print("Connecting to MongoDB...")
-        cluster = "mongodb://localhost:27017"
+        # print ("--------------------------------------------")
+        # print("Connecting to MongoDB...")
+        cluster = "mongodb+srv://adminuser:1234@cluster0.yrp2gy4.mongodb.net/?retryWrites=true&w=majority"
         client = MongoClient(cluster)
         # I could also have said "db = client.demo_database" to do the same thing.
         db = client.ParkingGarage
-        print("Successful")
+        # print("Successful")
         return db
